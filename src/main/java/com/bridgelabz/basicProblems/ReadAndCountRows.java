5f8da29d2968ac0d747804d78a39f3927b0bb55f
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-04-07 15:50:30 +0530

    [Siddharth] ADD: Count rows of CSV file

---
+++ src/main/java/com/bridgelabz/basicProblems/ReadAndCountRows.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.basicProblems;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ReadAndCountRows {
+    public static int countRows(String fileName) {
+        int numberOfRows = 0;
+
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+            String line;
+
+            while( (line = br.readLine()) != null){
+                numberOfRows++;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+       return numberOfRows;
+    }
+    public static void main(String[] args) {
+        String path = "D:\\8th sem\\bridgelabz.workspace\\java-csv\\src\\main\\java\\com\\bridgelabz\\basicProblems\\EmployeeDataOutput.csv";
+        System.out.println(countRows(path));;
+    }
+}
+//5
